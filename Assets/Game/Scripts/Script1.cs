179dc69f28c2ff7bd9e9d0a8d2f525e7095b074a
Radek Dlask <radekdlask@seznam.cz>
2017-05-05 21:24:56 +0200

    Initial commit

---
+++ Assets/Game/Scripts/Script1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        // here and there
+    }
+}
